619e3129d2abe6d84e5663aefb6ba308f0f78a39
Anastassakis <118926841+Anastassakis@users.noreply.github.com>
2025-07-04 23:58:12 -0300

    Add files via upload

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,41 @@
+public final class Calculadora {
+    /**
+     * Método para somar dois valores.
+     * @param valor1 primeiro valor da operação.
+     * @param valor2 segundo valor da operação.
+     * @return retorna o resultado da soma dos valores.
+     */
+    public double somar(final double valor1, final double valor2) {
+        return valor1 + valor2;
+    }
+
+    /**
+     * Método para subtrair dois valores.
+     * @param valor1 primeiro valor da operação.
+     * @param valor2 segundo valor da operação.
+     * @return retorna o resultado da subtração dos valores.
+     */
+    public double subtrair(final double valor1, final double valor2) {
+        return valor1 - valor2;
+    }
+
+    /**
+     * Método para multiplicar dois valores.
+     * @param valor1 primeiro valor da operação.
+     * @param valor2 segundo valor da operação.
+     * @return retorna o resultado da multiplicação dos valores.
+     */
+    public double multiplicar(final double valor1, final double valor2) {
+        return valor1 * valor2;
+    }
+
+    /**
+     * Método para dividir dois valores.
+     * @param valor1 primeiro valor da operação.
+     * @param valor2 segundo valor da operação.
+     * @return retorna o resultado da divisão dos valores.
+     */
+    public double dividir(final double valor1, final double valor2) {
+        return valor1 / valor2;
+    }
+}
